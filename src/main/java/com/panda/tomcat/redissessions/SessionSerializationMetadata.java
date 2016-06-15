c1250e4404a363686eeb775ebbdeeaf3a6846032
smileclj <842705148@qq.com>
2016-06-15 18:25:25 +0800

    commit code

---
+++ src/main/java/com/panda/tomcat/redissessions/SessionSerializationMetadata.java
@@ -0,0 +1,44 @@
+package com.panda.tomcat.redissessions;
+
+import java.io.IOException;
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+public class SessionSerializationMetadata implements Serializable {
+	private static final long serialVersionUID = -6294167612099182734L;
+	private byte[] sessionAttributesHash;
+
+	public SessionSerializationMetadata() {
+		this.sessionAttributesHash = new byte[0];
+	}
+
+	public byte[] getSessionAttributesHash() {
+		return sessionAttributesHash;
+	}
+
+	public void setSessionAttributesHash(byte[] sessionAttributesHash) {
+		this.sessionAttributesHash = sessionAttributesHash;
+	}
+
+	public void copyFieldsFrom(SessionSerializationMetadata metadata) {
+		this.setSessionAttributesHash(metadata.getSessionAttributesHash());
+	}
+
+	private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+		out.writeInt(sessionAttributesHash.length);
+		out.write(this.sessionAttributesHash);
+	}
+
+	private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+		int hashLength = in.readInt();
+		byte[] sessionAttributesHash = new byte[hashLength];
+		in.read(sessionAttributesHash, 0, hashLength);
+		this.sessionAttributesHash = sessionAttributesHash;
+	}
+
+	@SuppressWarnings("unused")
+	private void readObjectNoData() throws ObjectStreamException {
+		this.sessionAttributesHash = new byte[0];
+	}
+
+}
